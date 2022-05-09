1fd60b3a382f39b883644877854a9f1814150a43
vaishnavi chockalingam <68457139+vaishnavi-2103@users.noreply.github.com>
2022-05-10 05:11:24 +0530

    Update weather.java

--- lab5/weather.java
+++ lab5/weather.java
@@ -12,10 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author SRI GAYATHIRI
- */
 public class weather extends HttpServlet {
 
     /**

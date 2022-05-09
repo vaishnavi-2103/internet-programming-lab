32f277ea77a1ed02190691d0b612697fb6144891
vaishnavi chockalingam <68457139+vaishnavi-2103@users.noreply.github.com>
2022-05-10 05:10:33 +0530

    Update book.java

--- lab5/book.java
+++ lab5/book.java
@@ -12,10 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author VENMATHEE
- */
 class Book{
     String title;
     String author;

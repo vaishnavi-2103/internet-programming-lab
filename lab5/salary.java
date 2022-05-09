6268198903007bdb7f651fe0f7ba261a54649433
vaishnavi chockalingam <68457139+vaishnavi-2103@users.noreply.github.com>
2022-05-10 05:10:57 +0530

    Update salary.java

--- lab5/salary.java
+++ lab5/salary.java
@@ -11,10 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author SRI GAYATHIRI
- */
 public class Salary extends HttpServlet {
 
     /**
